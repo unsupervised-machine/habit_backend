e7172fc3d43ff85a4bf564e47ab558e65c777193
unsupervised-machine <taran.s.lau@gmail.com>
2025-02-26 19:21:17 -0800

    add cors middleware for local front end

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes import users, auth, habits, completions
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Allow frontend running on port 3000
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods
+    allow_headers=["*"],  # Allow all headers
+)
 
 # Include routes
 app.include_router(auth.router, prefix="/auth", tags=["Auth"])
