f386e9da7abd2a245fd81e80d35500bf796f4aa3
unsupervised-machine <taran.s.lau@gmail.com>
2025-02-28 00:25:15 -0800

    add local host to corsmiddleware origins

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Allow frontend running on port 3000
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],  # Allow frontend running on port 3000
     allow_credentials=True,
     allow_methods=["*"],  # Allow all HTTP methods
     allow_headers=["*"],  # Allow all headers
